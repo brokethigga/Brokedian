0018928eaadb0821a5ec7d09ccddb110b64f5130
brokethigga <ngu.ngi.ei.ei.ban@gmail.com>
2026-05-23 02:15:13 +0700

    translate to thai (WIP)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_VERSION = 'v12';
 const CACHE_NAME = `brokedian-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_NAME}-runtime`;
-const ASSETS = ['./', './index.html', './manifest.json', './styles.css?v=22'];
+const ASSETS = ['./', './index.html', './manifest.json', './styles.css?v=27'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
