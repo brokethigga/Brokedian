e0898c2b15573191e8ccdaf292a38695261401e0
brokethigga <ngu.ngi.ei.ei.ban@gmail.com>
2026-05-23 00:05:44 +0700

    Add languages button

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_VERSION = 'v12';
 const CACHE_NAME = `brokedian-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_NAME}-runtime`;
-const ASSETS = ['./', './index.html', './manifest.json', './styles.css?v=16'];
+const ASSETS = ['./', './index.html', './manifest.json', './styles.css?v=17'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
